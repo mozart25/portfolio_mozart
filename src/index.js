b76f0743a57cf8c0e832f95476185d466bb0bb81
mozart <mozart@watchmile.com>
2021-04-14 23:47:34 +0900

    add store, provider

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import './index.css';
 
 import App from './App';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
 );
